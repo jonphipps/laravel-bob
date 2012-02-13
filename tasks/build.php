498cc07b79329fbfc0cfad72d22d356255a47072
Dayle Rees <thepunkfan@gmail.com>
2012-02-13 21:50:04 +0000

    more prototypes

--- tasks/build.php
+++ tasks/build.php
@@ -9,17 +9,29 @@ class Bob_Build_Task extends Task
 	{
 		if(count($params) > 0)
 		{
-			$controller['name'] = $params[0];
-			$controller['lower'] = strtolower($params[0]);
-			$controller['class'] = Str::classify($controller['name'].'_Controller');
+			$map['1'] = Str::classify($params[0].'_Controller');
+			$map['2'] = strtolower($params[0]);
+
 			unset($params[0]);
-			$controller['actions'] = $params;
 
-			$controller_template = file_get_contents(path('bundle').'bob/templates/controller.tpl');
-			$controller_template = str_replace('%%NAME%%', $controller['class'], $controller_template);
+			echo "[+] Controller '{$map['1']}'\n\t- Action 'index'\n\t- View '{$map['2']}/index.blade.php'\n";
+			$actions = '';
+			foreach ($params as $action)
+			{
+				$actions .= $this->template_replace('action.tpl', array(
+					'1' => strtolower($action)
+				));
+				echo "\t- Action '" . strtolower($action) . "'\n\t- View '{$map['2']}/" . strtolower($action) . ".blade.php'\n";
+			}
 
-			file_put_contents(path('app').'controllers/'.$controller['lower'].EXT, $controller_template);
-			echo "Controller created.";
+			$map['3'] = $actions;
+
+			$template = $this->template_replace('controller.tpl', $map);
+
+			file_put_contents(path('app').'controllers/'.$map['2'].EXT, $template);
+
+			echo "YES WE CAN :D\n";
+			
 		}
 		else
 		{
@@ -30,5 +42,18 @@ class Bob_Build_Task extends Task
 	public function model($params)
 	{
 
+	}
+
+	private function template_replace($template, $map)
+	{
+		$path = path('bundle').'bob/templates/';
+		$template = file_get_contents($path.$template);
+
+		foreach($map as $key => $val)
+		{
+			$template = str_replace('%%'.$key.'%%', $val, $template);
+		}
+
+		return $template;
 	}
 }
---
+++ templates/action.tpl
@@ -0,0 +1,7 @@
+	public function %%1%%_action()
+	{
+		// code here..
+
+		return View::make('%%2%%.%%3%%');
+	}
+
